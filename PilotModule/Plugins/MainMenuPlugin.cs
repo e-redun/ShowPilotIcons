07e57d414fa641011130536cbefc9fe424b033e5
e-redun <e.redun@mail.ru>
2025-02-15 23:24:56 +0500

    Добавьте файлы проекта.

---
+++ PilotModule/Plugins/MainMenuPlugin.cs
@@ -0,0 +1,36 @@
+﻿using Ascon.Pilot.SDK;
+using Ascon.Pilot.SDK.Menu;
+using Ascon.Pilot.Theme.Controls;
+using System.Composition;
+
+namespace PilotModule.Plugins
+{
+    [Export(typeof(IMenu<MainViewContext>))]
+    public class MainMenuPlugin : IMenu<MainViewContext>
+    {
+        private const string SHOW_ICONS = "ShowIcons";
+
+        [ImportingConstructor]
+        public MainMenuPlugin()
+        {
+        }
+
+        public void Build(IMenuBuilder builder, MainViewContext context)
+        {
+            builder.AddItem(SHOW_ICONS, builder.Count).WithHeader(SHOW_ICONS);
+        }
+
+        public void OnMenuItemClick(string name, MainViewContext context)
+        {
+            if (name == SHOW_ICONS)
+            {
+                var iconsView = new IconsView();
+
+                var baseWindow = new DialogWindow();
+                baseWindow.Content = iconsView;
+
+                baseWindow.ShowDialog();
+            }
+        }
+    }
+}
---
+++ PilotModule/Views/IconsView.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace PilotModule.Views
+{
+    public partial class IconsView
+    {
+        public ICommand GetIconFullNameToClipboardCommand { get; set; }
+
+        public IconsView()
+        {
+            InitializeComponent();
+        }
+
+        private void GetIconFullNameToClipboard_Button_Click(object sender, RoutedEventArgs e)
+        {
+            var button = sender as Button;
+
+            if (button == null)
+            {
+                return;
+            }
+
+            var d = (DictionaryEntry)button.DataContext;
+
+            var text = "Icons.Instance." + d.Key;
+
+            Clipboard.SetText(text);
+        }
+    }
+}
